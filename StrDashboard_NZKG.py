bdbe574c340d3a210a2ba24cdbf4c5c22d048fd5
LizaOsakue <147401866+LizaOsakue@users.noreply.github.com>
2024-03-12 12:37:13 +0100

    Update StrDashboard_NZKG.py

--- StrDashboard_NZKG.py
+++ StrDashboard_NZKG.py
@@ -1,5 +1,14 @@
+from pathlib import Path
+
 import appdirs as ad
-ad.user_cache_dir = lambda *args: "/tmp"
+
+CACHE_DIR = ".cache"
+
+# Force appdirs to say that the cache dir is .cache
+ad.user_cache_dir = lambda *args: CACHE_DIR
+
+# Create the cache dir if it doesn't exist
+Path(CACHE_DIR).mkdir(exist_ok=True)
 
 
 from collections import UserString
